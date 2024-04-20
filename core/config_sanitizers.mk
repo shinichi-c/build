RisingTechOSS/android_build

build: make: Restore 32bit libc scudo support
Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

core/config_sanitizers.mk
@@ -299,8 +299,8 @@ ifneq ($(filter address thread hwaddress,$(my_sanitize)),)
 endif
 
 # Or if disabled globally.
-ifneq ($(PRODUCT_USE_SCUDO),true)
-  my_sanitize := $(filter-out scudo,$(my_sanitize))
+ifeq ($(and $(filter true,$(PRODUCT_USE_SCUDO)),$(filter true,$(PRODUCT_USE_SCUDO_32_BIT))),)
+    my_sanitize := $(filter-out scudo,$(my_sanitize))
 endif
 
 # Undefined symbols can occur if a non-sanitized library links

shinichi-c/build

Change-Id: I59492499dc3833dbb16d56fec1e5666a18613b22

core/config_sanitizers.mk
@@ -322,8 +322,8 @@ ifneq ($(filter address thread hwaddress,$(my_sanitize)),)
 endif
 
 # Or if disabled globally.
-ifneq ($(PRODUCT_USE_SCUDO),true)
-  my_sanitize := $(filter-out scudo,$(my_sanitize))
+ifneq ($(and $(filter true,$(PRODUCT_USE_SCUDO)),$(filter true,$(PRODUCT_USE_SCUDO_32_BIT))),)
+    my_sanitize := $(filter-out scudo,$(my_sanitize))
 endif
 
 # Undefined symbols can occur if a non-sanitized library links